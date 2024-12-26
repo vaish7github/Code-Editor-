2b93835781405cf7585752caba57c899ce08ab19
Vaishnavi Tiwari <vaishnavitiwari968@gmail.com>
2024-12-26 12:25:30 +0530

    First Copy from devStudio

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,7 @@ import {defineSchema, defineTable} from "convex/server"
 import {v} from "convex/values"
 export default defineSchema({
     users : defineTable({
-        userId : v.string(), 
+        userId : v.string(),  // clerkId
         email : v.string() , 
         isPro : v.boolean() , 
         proSince : v.optional(v.number()) , 
