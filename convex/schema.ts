e28d8a248c178f455967c8a9beec17524141b765
Vaishnavi Tiwari <vaishnavitiwari968@gmail.com>
2025-01-04 19:22:34 +0530

    1st pr

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,6 +3,7 @@ import {v} from "convex/values"
 export default defineSchema({
     users : defineTable({
         userId : v.string(), 
+        
         email : v.string() , 
         isPro : v.boolean() , 
         proSince : v.optional(v.number()) , 
