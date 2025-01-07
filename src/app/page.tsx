2154ddbf3d29a81591b039cfd67c4663bec5ee81
Vaishnavi Tiwari <vaishnavitiwari968@gmail.com>
2025-01-08 00:21:41 +0530

    The footer has been added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import {SignOutButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignUpButton>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Signup
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <UserButton/>
-      <SignedIn>
-        <SignOutButton>
-          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Signout
-          </button>
-        </SignOutButton>
-      </SignedIn>
+      hey 
     </div>
   )
 }
